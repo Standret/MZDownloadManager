fe49e5b8e3144b4c9d9cf388bc08d99f19f31e2a
Standret <pstandret@gmail.com>
2019-01-08 14:46:36 +0200

    fixed resume

---
+++ Example/build/Pods.build/Debug-iphoneos/MZDownloadManager.build/Objects-normal/arm64/MZDownloadManager-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/startupsoft/Documents/Petro/Xcode/MZDownloadManager/Example/Pods/Target\ Support\ Files/MZDownloadManager/MZDownloadManager-dummy.m \
+  /Users/startupsoft/Documents/Petro/Xcode/MZDownloadManager/Example/build/Pods.build/Debug-iphoneos/MZDownloadManager.build/module.modulemap \
+  /Users/startupsoft/Documents/Petro/Xcode/MZDownloadManager/Example/Pods/Target\ Support\ Files/MZDownloadManager/MZDownloadManager-prefix.pch
---
+++ Example/build/Pods.build/Debug-iphoneos/MZDownloadManager.build/Objects-normal/arm64/MZDownloadManager_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/startupsoft/Documents/Petro/Xcode/MZDownloadManager/Example/build/Pods.build/Debug-iphoneos/MZDownloadManager.build/DerivedSources/MZDownloadManager_vers.c
